2d4b999880207f87b4acdaf22ec67ab936801524
Jean Turgeon <jean_turgeon@ymail.com>
2023-11-15 16:01:01 -0400

    added styling sheet

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 //
 //
+use gdk::prelude::*;
 use gtk::prelude::*;
 use gtk::{ApplicationWindow, Builder};
+use gtk::{CssProvider, StyleContext};
 
 fn main() {
+    if gtk::init().is_err() {
+        println!("ERROR: Failed to initialize GTK.");
+        return;
+    }
+
     let application =
         gtk::Application::new(Some("com.github.jean-turgeon.rusted"), Default::default());
 
@@ -13,13 +20,40 @@ fn main() {
 }
 
 fn build_ui(application: &gtk::Application) {
-    let glade_src = include_str!("rusted.glade");
+    let glade_src = include_str!("./rusted.glade");
     let builder = Builder::from_string(glade_src);
 
-    let window: ApplicationWindow = builder.object("mainWindow").expect("Couldn't get window1");
+    let window: ApplicationWindow = builder
+        .object("mainWindow")
+        .expect("ERROR: Couldn't get window!");
+
+    let provider = CssProvider::new();
+    provider.load_from_path("./rusted.css").unwrap();
+
+    let _context = window.style_context();
+    StyleContext::add_provider_for_screen(
+        &gdk::Screen::default().unwrap(),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
     window.set_application(Some(application));
 
+    //window.connect_delete_event(|_, _| {
+    //    gtk::main_quit();
+    //    gtk::signal::Inhibit(false)
+    //});
+
+    // fetch manifest information
+    //let authors: &str = env!("CARGO_PKG_AUTHORS");
+    //let description: &str = env!("CARGO_PKG_DESCRIPTION");
+    //let name: &str = env!("CARGO_PKG_NAME");
+    //let version: &str = env!("CARGO_PKG_VERSION");
+
+    window.maximize();
     window.show_all();
+
+    gtk::main();
 }
 
 #[cfg(test)]
